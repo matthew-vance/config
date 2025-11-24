f84d45384addd440d6814e23fffda530aaa50229
Matthew Vance <mavance44@gmail.com>
2025-11-24 11:51:57 -0500

    migrate starship config to home manager

--- nix/dot-config/nix/home/matthew.nix
+++ nix/dot-config/nix/home/matthew.nix
@@ -3,48 +3,221 @@
 {
   home.stateVersion = "25.05";
 
-  programs.home-manager.enable = true;
-
-  programs.git = {
-    enable = true;
-    settings = {
-      commit.gpgsign = true;
-      core.pager = "delta --line-numbers";
-      delta = {
-        hyperlinks = true;
-        interactive.keep-plus-minus-markers = false;
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        commit.gpgsign = true;
+        core.pager = "delta --line-numbers";
+        delta = {
+          hyperlinks = true;
+          interactive.keep-plus-minus-markers = false;
+        };
+        diff.colorMoved = "default";
+        filter.lfs = {
+          clean = "git-lfs clean -- %f";
+          smudge = "git-lfs smudge -- %f";
+          process = "git-lfs filter-process";
+          required = true;
+        };
+        gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+        init.defaultBranch = "main";
+        interactive.diffFilter = "delta --color-only --features=interactive";
+        merge.conflictStyle = "diff3";
+        pull.rebase = "false";
+        push.autoSetupRemote = true;
+        user = {
+          name = "Matthew Vance";
+          email = "mavance44@gmail.com";
+        };
       };
-      diff.colorMoved = "default";
-      filter.lfs = {
-        clean = "git-lfs clean -- %f";
-        smudge = "git-lfs smudge -- %f";
-        process = "git-lfs filter-process";
-        required = true;
-      };
-      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-      init.defaultBranch = "main";
-      interactive.diffFilter = "delta --color-only --features=interactive";
-      merge.conflictStyle = "diff3";
-      pull.rebase = "false";
-      push.autoSetupRemote = true;
-      user = {
-        name = "Matthew Vance";
-        email = "mavance44@gmail.com";
+      signing = {
+        format = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/G8WN10/FlsokXrjIJ2C7Ev70Q8OL66dSfRXmP94hU";
       };
     };
-    signing = {
-      format = "ssh";
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/G8WN10/FlsokXrjIJ2C7Ev70Q8OL66dSfRXmP94hU";
-    };
-  };
 
-  programs.ghostty = {
-    enable = true;
-    package = null; # Use homebrew version
-    settings = {
-      font-size = 14;
-      font-family = "JetBrains Mono";
-      theme = "Night Owl";
+    ghostty = {
+      enable = true;
+      package = null; # Use homebrew version
+      settings = {
+        font-size = 14;
+        font-family = "JetBrains Mono";
+        theme = "Night Owl";
+      };
+    };
+
+    home-manager.enable = true;
+
+    starship = {
+      enable = true;
+      settings = {
+        aws = {
+          symbol = "  ";
+        };
+        buf = {
+          symbol = " ";
+        };
+        c = {
+          symbol = " ";
+        };
+        conda = {
+          symbol = " ";
+        };
+        crystal = {
+          symbol = " ";
+        };
+        dart = {
+          symbol = " ";
+        };
+        directory = {
+          read_only = " 󰌾";
+        };
+        docker_context = {
+          symbol = " ";
+        };
+        elixir = {
+          symbol = " ";
+        };
+        elm = {
+          symbol = " ";
+        };
+        fennel = {
+          symbol = " ";
+        };
+        fossil_branch = {
+          symbol = " ";
+        };
+        git_branch = {
+          symbol = " ";
+        };
+        golang = {
+          symbol = " ";
+        };
+        guix_shell = {
+          symbol = " ";
+        };
+        haskell = {
+          symbol = " ";
+        };
+        haxe = {
+          symbol = " ";
+        };
+        hg_branch = {
+          symbol = " ";
+        };
+        hostname = {
+          ssh_symbol = " ";
+        };
+        java = {
+          symbol = " ";
+        };
+        julia = {
+          symbol = " ";
+        };
+        kotlin = {
+          symbol = " ";
+        };
+        lua = {
+          symbol = " ";
+        };
+        memory_usage = {
+          symbol = "󰍛 ";
+        };
+        meson = {
+          symbol = "󰔷 ";
+        };
+        nim = {
+          symbol = "󰆥 ";
+        };
+        nix_shell = {
+          symbol = " ";
+        };
+        nodejs = {
+          symbol = " ";
+        };
+        ocaml = {
+          symbol = " ";
+        };
+        os.symbols = {
+          Alpaquita = " ";
+          Alpine = " ";
+          AlmaLinux = " ";
+          Amazon = " ";
+          Android = " ";
+          Arch = " ";
+          Artix = " ";
+          CentOS = " ";
+          Debian = " ";
+          DragonFly = " ";
+          Emscripten = " ";
+          EndeavourOS = " ";
+          Fedora = " ";
+          FreeBSD = " ";
+          Garuda = "󰛓 ";
+          Gentoo = " ";
+          HardenedBSD = "󰞌 ";
+          Illumos = "󰈸 ";
+          Kali = " ";
+          Linux = " ";
+          Mabox = " ";
+          Macos = " ";
+          Manjaro = " ";
+          Mariner = " ";
+          MidnightBSD = " ";
+          Mint = " ";
+          NetBSD = " ";
+          NixOS = " ";
+          OpenBSD = "󰈺 ";
+          openSUSE = " ";
+          OracleLinux = "󰌷 ";
+          Pop = " ";
+          Raspbian = " ";
+          Redhat = " ";
+          RedHatEnterprise = " ";
+          RockyLinux = " ";
+          Redox = "󰀘 ";
+          Solus = "󰠳 ";
+          SUSE = " ";
+          Ubuntu = " ";
+          Unknown = " ";
+          Void = " ";
+          Windows = "󰍲 ";
+        };
+        package = {
+          symbol = "󰏗 ";
+        };
+        perl = {
+          symbol = " ";
+        };
+        php = {
+          symbol = " ";
+        };
+        pijul_channel = {
+          symbol = " ";
+        };
+        python = {
+          symbol = " ";
+        };
+        rlang = {
+          symbol = "󰟔 ";
+        };
+        ruby = {
+          symbol = " ";
+        };
+        rust = {
+          symbol = "󱘗 ";
+        };
+        scala = {
+          symbol = " ";
+        };
+        swift = {
+          symbol = " ";
+        };
+        zig = {
+          symbol = " ";
+        };
+      };
     };
   };
 }
