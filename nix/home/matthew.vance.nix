0e71dc04c81ae1c68d6c4b178f6273a7432c74ab
Matthew Vance <mavance44@gmail.com>
2025-12-04 09:00:46 -0500

    add sdkman source to taillight config

--- nix/home/matthew.vance.nix
+++ nix/home/matthew.vance.nix
@@ -1,14 +1,23 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   imports = [ ./common.nix ];
 
-  xdg.configFile."git/taillight.local.gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/config/git/taillight.local.gitconfig";
+  xdg.configFile."git/taillight.local.gitconfig".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/config/git/taillight.local.gitconfig";
 
-  programs.git.includes = [
-    {
-      condition = "gitdir:~/code/taillight/";
-      path = "${config.xdg.configHome}/git/taillight.local.gitconfig";
-    }
-  ];
+  programs = {
+    git.includes = [
+      {
+        condition = "gitdir:~/code/taillight/";
+        path = "${config.xdg.configHome}/git/taillight.local.gitconfig";
+      }
+    ];
+
+    zsh = {
+      initContent = lib.mkOrder 1500 ''
+        [[ -s "${config.home.homeDirectory}/.sdkman/bin/sdkman-init.sh" ]] && source "${config.home.homeDirectory}/.sdkman/bin/sdkman-init.sh"
+      '';
+    };
+  };
 }
